be18574bd9180642a98401883543f5c737809c61
Luis Flores <luisfloresvid@gmail.com>
2023-05-31 06:35:29 +0000

    Updated list.component.html to show a more descriptive placeholder and list.component.ts in the search method to find the Pokemon whether the user enters uppercase or lowercase text

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { NamedAPIResource, NamedAPIResourceList } from 'pokenode-ts';
 import { Sort } from 'src/app/models/search-sort';
 import { PaginationService } from 'src/app/services/pagination/pagination.service';
 import { PokemonService } from 'src/app/services/pokemon/pokemon.service';
@@ -8,14 +7,14 @@ import { PokemonService } from 'src/app/services/pokemon/pokemon.service';
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.scss']
+  styleUrls: ['./list.component.scss'],
 })
 export class ListComponent {
   constructor(
-    private pokemonService: PokemonService, 
+    private pokemonService: PokemonService,
     private paginationService: PaginationService,
     private router: Router
-    ){}
+  ) {}
 
   pagination: any;
   list: any;
@@ -27,12 +26,15 @@ export class ListComponent {
   sort: Sort = Sort.Default;
 
   ngOnInit() {
-    this.getPokemonList(this.offset, this.limit)
+    this.getPokemonList(this.offset, this.limit);
   }
 
-  getPokemonList(offset:number, limit:number) {
-    this.pokemonService.getPokemonList(offset, limit).then(response => {
-      this.pagination = this.paginationService.getPagination(response.count, limit);
+  getPokemonList(offset: number, limit: number) {
+    this.pokemonService.getPokemonList(offset, limit).then((response) => {
+      this.pagination = this.paginationService.getPagination(
+        response.count,
+        limit
+      );
       this.list = response.results;
       this.filterableList = this.list;
       this.loading = false;
@@ -41,30 +43,36 @@ export class ListComponent {
 
   changePage(page: number) {
     this.searchText = '';
-    this.pagination = this.paginationService.getPagination(this.pagination.pages, this.limit, page);
-    this.getPokemonList(this.pagination.offset, this.limit)
+    this.pagination = this.paginationService.getPagination(
+      this.pagination.pages,
+      this.limit,
+      page
+    );
+    this.getPokemonList(this.pagination.offset, this.limit);
   }
 
   navigateToDetail(name: string) {
-    this.router.navigate([`/pokemon/${name}`])
+    this.router.navigate([`/pokemon/${name}`]);
   }
 
   searchOnList() {
-    this.filterableList = this.list.filter((pokemon: any) => pokemon.name.includes(this.searchText))
+    this.filterableList = this.list.filter((pokemon: any) =>
+      pokemon.name.includes(this.searchText.toLowerCase())
+    );
   }
 
   sortList() {
-    this.filterableList = this.list.sort((n1: any,n2: any) => {
-      debugger
+    this.filterableList = this.list.sort((n1: any, n2: any) => {
+      debugger;
       if (n1.name < n2.name) {
         this.sort = Sort.DESC;
         return -1;
       }
       if (n1.name > n2.name) {
-        this.sort = Sort.ASC
+        this.sort = Sort.ASC;
         return 1;
       }
       return 0;
-  })
+    });
   }
 }
